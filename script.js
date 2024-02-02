70b083c2ed87833464f5a854b1c5e7dd33f95633
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2024-02-03 01:09:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 //your JS code here. If required.
 
 const Btn = document.getElementById("btn");
-comst Div = document.getElementById("output");
+const Div = document.getElementById("output");
 
 Btn.addEventListener("click",async () => {
 const Text = document.getElementById("text").value;
 const Delay = document.getElementById("delay").value;
 	if(Text && Delay){
 		let prom = new Promise((res,rej) => {
-			setTimeOut(() => {
+			setTimeout(() => {
 				res(Text);
 			},Delay);
 		});
 		let x = await prom;
 		if(x){
-			Div.innerText = "Text";
+			Div.innerText = Text;
 		}
 	}
-})
+});
